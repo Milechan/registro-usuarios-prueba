0c174aef36ebab6b7a378b0fcbf1958c1e1d8fbb
milena <milena.concha.m@gmail.com>
2025-06-15 00:13:45 -0400

    se agrega cors

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,16 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route('/registro', methods=['POST'])
-def registro_usuario():
+@app.route('/register', methods=['POST'])
+def register_user():
     # valida que en el body venga un json
     if request.content_type != 'application/json':
         return jsonify({'error': 'Debe enviarse un JSON en el body'}), 400
